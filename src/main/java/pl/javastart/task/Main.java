a959d52ed4e80f8c92c523ea3663e949ab1868d5
Jan Termion <jan.termion@gmail.com>
2023-02-04 13:31:56 +0100

    zadanie zrobione

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Room room1 = new Room(true, 34, 21, 20.5);
+        Room room2 = new Room(false, 72, 25, 19.5);
+        Room room3 = new Room(true, 15, 28, 18.0);
+
+        Print print = new Print();
+
+        print.roomIformation(room1);
+        System.out.println("Czy udało się obniżyć temperaturę w pomieszczeniu?: " + room1.lowerTemperature());
+        print.roomIformation(room1);
+
+        print.roomIformation(room2);
+        System.out.println("Czy udało się obniżyć temperaturę w pomieszczeniu?: " + room2.lowerTemperature());
+        print.roomIformation(room2);
+
+        print.roomIformation(room3);
+        System.out.println("Czy udało się obniżyć temperaturę w pomieszczeniu?: " + room3.lowerTemperature());
+        print.roomIformation(room3);
     }
 }
---
+++ src/main/java/pl/javastart/task/Print.java
@@ -0,0 +1,9 @@
+package pl.javastart.task;
+
+public class Print {
+
+    void roomIformation(Room room) {
+        System.out.printf("wielkość pomieszczenia w m2: %.0f m2\n", room.getRoomSize());
+        System.out.printf("temperatura pomieszczenia aktualna: %.1f C\n\n", room.getRoomTemperature());
+    }
+}
