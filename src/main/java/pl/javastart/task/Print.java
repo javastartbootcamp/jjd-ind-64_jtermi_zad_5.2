aeb0d6e5fd5bf3ef98a0c12d4541c676ae00ca80
Jan Termion <jan.termion@gmail.com>
2023-02-04 13:33:51 +0100

    zadanie zrobione

--- src/main/java/pl/javastart/task/Print.java
+++ src/main/java/pl/javastart/task/Print.java
@@ -4,6 +4,6 @@ public class Print {
 
     void roomIformation(Room room) {
         System.out.printf("wielkość pomieszczenia w m2: %.0f m2\n", room.getRoomSize());
-        System.out.printf("temperatura pomieszczenia aktualna: %.1f C\n\n", room.getRoomTemperature());
+        System.out.printf("temperatura pomieszczenia aktualna: %.1f*C\n\n", room.getRoomTemperature());
     }
 }
